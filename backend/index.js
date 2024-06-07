bbc5c08b9a9d0231c88d50323a8ea313527f36df
Akshat <akshatdwivedi59941@gmail.com>
2024-06-08 01:11:49 +0530

    Added Some package

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+const app=express();
+
+dotenv.config();
+const port=process.env.PORT;
+
+const connect=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODBURL);
+        console.log('Got Connected to the Db ');
+    } catch (error) {
+        console.log('ewww');
+        console.log(error);
+    }
+}
+mongoose.connection.on("disconnected",()=>{
+    console.log("disconnect ho gya bsdk ");
+})
+mongoose.connection.on("connected",()=>{
+    console.log("bhai ! bas kr connect ho gya ");
+})
+app.listen(port,()=>{
+    console.log('Connected to '+ port);
+    connect();
+})
